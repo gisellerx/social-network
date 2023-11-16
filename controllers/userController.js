297c74575a640f128a2fa70d55a3899ee986bf9c
gisellerx <giselle.armas@outlook.com>
2023-11-16 18:20:30 -0500

    added update and delete user options to controllers/routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Thought = require('../models/Thought');
 
 module.exports = {
   async getUsers(req, res) {
@@ -32,4 +33,46 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async updateUser(req, res) {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const thought = await Thought.findOneAndRemove({ username: req.body.username });
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      const user = await User.findOneAndRemove(
+        { _id: req.params.userId },
+        { new: true }
+      );
+
+      if (!user) {
+        return res
+          .status(404)
+          .json({ message: 'Video created but no user with this id!' });
+      }
+
+      res.json({ message: 'Video successfully deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
 };
